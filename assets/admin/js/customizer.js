d523db15c41b93c52d1de4601a525e6ad8f89f39
Mahdi Yazdani <mahdiyazdani@mail.com>
2017-12-14 16:59:01 +0400

    Added hide site tagline method

--- assets/admin/js/customizer.js
+++ assets/admin/js/customizer.js
@@ -2,20 +2,38 @@
  * Theme Customizer enhancements for a better user experience.
  * Contains handlers to make Theme Customizer preview reload changes asynchronously.
  *
- * @author  	Mahdi Yazdani
- * @package 	Siuy
- * @since 	    1.0.0
+ * @author      Mahdi Yazdani
+ * @package     Siuy
+ * @since       1.1.0
  */
-(function($) {
-    // Site title and description.
+(function(window, $, undefined) {
+    "use strict";
+    // Update site title
     wp.customize('blogname', function(value) {
         value.bind(function(to) {
             $('.site-title a').text(to);
         });
     });
+    // Update site tagline content
     wp.customize('blogdescription', function(value) {
         value.bind(function(to) {
             $('.site-description').text(to);
+        });
+    });
+    // Hide site tagline
+    wp.customize('siuy_toggle_site_tagline', function(value) {
+        value.bind(function(to) {
+            if (true === to) {
+                $('.site-description').css({
+                    'clip': 'rect(1px, 1px, 1px, 1px)',
+                    'position': 'absolute'
+                });
+            } else {
+                $('.site-description').css({
+                    'clip': 'auto',
+                    'position': 'relative'
+                });
+            }
         });
     });
     // Header text color.
@@ -37,4 +55,21 @@
             }
         });
     });
-})(jQuery);
+    /**
+     * "Layout" section
+     * General Layout
+     *
+     * @since 1.1.0
+     */
+    wp.customize('siuy_layout_sidebar', function(value) {
+        value.bind(function(to) {
+            if ('left-sidebar' === to && ! $('body').hasClass('no-sidebar')) {
+                $('body').removeClass('right-sidebar');
+                $('body').addClass('left-sidebar');
+            } else if ('right-sidebar' === to && ! $('body').hasClass('no-sidebar')) {
+                $('body').removeClass('left-sidebar');
+                $('body').addClass('right-sidebar');
+            }
+        });
+    });
+})(this, jQuery);
