d18a1c8803afe4a2197a285ca0fad15a3f6336f4
Mahdi Yazdani <mahdiyazdani@mail.com>
2017-12-19 14:48:20 +0400

    Fixed syntax error

--- template-parts/content-image.php
+++ template-parts/content-image.php
@@ -18,6 +18,7 @@
 			<?php if (! is_singular()): ?>
 			<a href="<?php the_permalink(); ?>" target="_self">
 			<?php
+			endif;
 			if (has_post_thumbnail()):
 				the_post_thumbnail('siuy-featured-image', array('itemprop' => 'image')); 
 			elseif (!has_post_thumbnail() && !empty($get_images)):
