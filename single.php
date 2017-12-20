1e85e418bf36625664bd6846cbfbdc06f6a7cba3
Mahdi Yazdani <mahdiyazdani@mail.com>
2017-12-20 16:46:13 +0400

    Added `get_post_format` support

--- single.php
+++ single.php
@@ -15,7 +15,22 @@ get_header(); ?>
 		<?php
 		while (have_posts()) : the_post();
 
-			get_template_part('template-parts/content', get_post_type());
+			if ('post' === get_post_type()):
+				/*
+				 * Include the Post-Format-specific template for the content.
+				 * If you want to override this in a child theme, then include a file
+				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+				 */
+				get_template_part('template-parts/content', get_post_format());
+				
+			else:
+				/*
+				 * Retrieves the post type of the current post or of a given post.
+				 * called single-___.php (where ___ is the Post Type name) and that will be used instead.
+				 */
+				get_template_part('template-parts/content', get_post_type());
+
+			endif;
 
 			the_post_navigation();
 
