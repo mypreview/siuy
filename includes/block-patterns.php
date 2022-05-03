b34476066aba47a5ed8597faf8d86b41f6872837
Mahdi Yazdani <mahdiyazdani@msn.com>
2022-05-03 14:04:40 +0300

    FPO

---
+++ includes/block-patterns.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Block patterns.
+ *
+ * @link          https://mypreview.github.io/siuy
+ * @author        MyPreview (Github: @mahdiyazdani, @gooklani, @mypreview)
+ * @since         2.0.0
+ *
+ * @package       siuy
+ * @subpackage    siuy/includes
+ * @phpcs:disable WPThemeReview.PluginTerritory.ForbiddenFunctions.editor_blocks_register_block_style
+ */
+
+namespace Siuy\Includes\Block_Patterns;
+
+defined( 'ABSPATH' ) || exit; // Exit if accessed directly.
---
+++ includes/block-styles.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Block styles.
+ *
+ * @link          https://mypreview.github.io/siuy
+ * @author        MyPreview (Github: @mahdiyazdani, @gooklani, @mypreview)
+ * @since         2.0.0
+ *
+ * @package       siuy
+ * @subpackage    siuy/includes
+ * @phpcs:disable WPThemeReview.PluginTerritory.ForbiddenFunctions.editor_blocks_register_block_style
+ */
+
+namespace Siuy\Includes\Block_Styles;
+
+defined( 'ABSPATH' ) || exit; // Exit if accessed directly.
