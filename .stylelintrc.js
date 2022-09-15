56f9b712e8984028982389b7727582b0a05914f8
Mahdi Yazdani <mahdiyazdani@msn.com>
2022-09-15 14:40:31 +0300

    Exclude additional rules

---
+++ .stylelintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+	extends: '@wordpress/stylelint-config/scss',
+	rules: {
+		'at-rule-empty-line-before': null,
+		'at-rule-no-unknown': null,
+		'font-family-no-missing-generic-family-keyword': null,
+	},
+};
